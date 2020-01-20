bb2fd01ff10774e7e0e8e367ac41aa3cec52e052
Tetiana Novakivska <tn@encode.dk>
2020-01-20 19:07:54 +0200

    Updated

--- src/main/java/GameFrame.java
+++ src/main/java/GameFrame.java
@@ -2,24 +2,25 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class HelloWorld {
+public class GameFrame {
     private JPanel rootPanel;
     private JLabel helloLabel;
-    private JButton pressMeButton;
+    private JButton PressMeButton;
     private JTextField nameTextField;
+    private JButton PictureOneButton;
 
-    public HelloWorld() {
-        pressMeButton.addActionListener(new ActionListener() {
+    public GameFrame() {
+        PressMeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
-                String greeting = "Hello " + nameTextField.getText();
+                String greeting = "Hello, " + nameTextField.getText() + "! Let's find a cat!";
                 helloLabel.setText(greeting);
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("HelloWorld");
-        frame.setContentPane(new HelloWorld().rootPanel);
+        JFrame frame = new JFrame("GameFrame");
+        frame.setContentPane(new GameFrame().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
--- src/main/java/game/FindACat.java
+++ src/main/java/game/FindACat.java
@@ -3,4 +3,8 @@ package game;
 public class FindACat {
 
 
+public static void showGame(){
+
+
+}
 }
